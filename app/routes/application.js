55f939db022baca4473276fb0a266cbf7e495e72
sekhar karri <sekhar.karri.ext@lvs.co.uk>
2017-06-29 16:02:44 +0100

    Auth integration

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
+import get from 'ember-metal/get';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-export default Ember.Route.extend({
-
+export default Ember.Route.extend(ApplicationRouteMixin,{
+session: Ember.inject.service(),
+beforeModel: function() {
+	const session = get(this,'session');
+	if(get(session, 'isAuthenticated')){
+		return get(this, 'session').getCurrentUser();
+	}
+},
 	actions: {
 		login(){
 			let {username, password} = this.getProperties('username', 'passsword');
---
+++ app/services/session.js
@@ -0,0 +1,58 @@
+import Session from 'ember-simple-auth/services/session';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import service from 'ember-service/inject';
+import computed from 'ember-computed';
+
+export default Session.extend({
+  ajax: service(),
+  store: service(),
+
+  hasUser: computed('isAuthenticated', 'account', function() {
+    return get(this, 'isAuthenticated') && get(this, 'account');
+  }).readOnly(),
+
+  authenticateWithOAuth2(identification, password) {
+    return this.authenticate('authenticator:oauth2', identification, password);
+  },
+
+  authenticateWithFacebook() {
+    return this.authenticate('authenticator:assertion', 'facebook');
+  },
+
+  /**
+   * Determine if the user is the same as the sessioned user.
+   */
+  isCurrentUser(user) {
+    const hasUser = get(this, 'hasUser');
+    if (!hasUser || !user) { return false; }
+    const sessionId = get(this, 'account.id');
+    const userId = get(user, 'id') || user;
+    return sessionId === userId;
+  },
+
+  /**
+   * Get the account information for the sessioned user
+   */
+  getCurrentUser() {
+    const requestUrl = '/users?filter[self]=true&include=userRoles.role';
+    return get(this, 'ajax').request(requestUrl).then((response) => {
+      // push the user data into the store
+      const [data] = response.data;
+      const normalizedData = get(this, 'store').normalize('user', data);
+      const user = get(this, 'store').push(normalizedData);
+      // push any included data into the store
+      const included = response.included || [];
+      included.forEach((record) => {
+        let type = get(record, 'type');
+        type = type === 'userRoles' ? 'user-role' : 'role';
+        get(this, 'store').push(get(this, 'store').normalize(type, record));
+      });
+      // set reference of the user
+      set(this, 'account', user);
+      return user;
+    }).catch(() => {
+      this.invalidate();
+    });
+  }
+});
